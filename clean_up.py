0ecec1573447321d80d270073054e041f6aa8a63
w.ksiazak <weronikaksiazak@gmail.com>
2020-05-09 16:19:48 +0200

    dodaje clean_up v2

--- clean_up.py
+++ clean_up.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 dane = pd.read_csv('Adult_train.tab', sep = "\t")
 
-print(dane)
+pd.set_option('display.max_columns', None)
+
+print(dane.dtypes)
+print(dane.isnull().count())
+print(dane.describe())
+print(dane.shape)
+
+dane = dane.replace("?", pd.np.nan)
+print(dane.isnull().sum())
