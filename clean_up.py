3760cb50855b7665ee742e8903a75c3b958a4156
mgorzalczany <mateusz.gorzalczany@gmail.com>
2020-05-10 11:41:15 +0200

    mapowanie v.3

--- clean_up.py
+++ clean_up.py
@@ -28,17 +28,6 @@ print(dane.dtypes)
 
 #MAPOWANIE#
 
-''' def splitDatasetIntoTrainAndTest(self, X, y, train_split_percent = 0.6):
-        # pd.set_option('display.max_columns', None)
-        # print(X)
-        print(X.info())
-        # print(X.describe())
-        # print(X.describe(include=[pd.np.number]))
-        # print(X.describe(include=[pd.np.object]))
-        # print(X.describe(include=['category']))
-        # print(X.describe(include={'boolean'}))
-        X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=train_split_percent)
-        return X_train, X_test, y_train, y_test '''
 class Classifiers:
     def datasetPreprocessing(self, X, columns_to_map):
         X_clean=X
@@ -98,5 +87,7 @@ X_clean = c.datasetPreprocessing(
 print(X_clean.dtypes)
 
 X_train, X_test, y_train, y_test = c.splitDatasetIntoTrainAndTest(
-      X=X_clean,
-      y=X_clean['age'])
+      X=X_clean.drop(columns=['y']),
+      y=X_clean['y'])
+print(X_train)
+print(y_train)
