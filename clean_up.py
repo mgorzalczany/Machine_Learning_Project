4e2217f28916d6503884d30a9ac5fe78867ccaa9
w.ksiazak <weronikaksiazak@gmail.com>
2020-05-10 10:27:41 +0200

    mapowanie 2

--- clean_up.py
+++ clean_up.py
@@ -24,6 +24,42 @@ print(dane.dtypes)
 
 
 
+
+#MAPOWANIE#
+
+''' def splitDatasetIntoTrainAndTest(self, X, y, train_split_percent = 0.6):
+        # pd.set_option('display.max_columns', None)
+        # print(X)
+        print(X.info())
+        # print(X.describe())
+        # print(X.describe(include=[pd.np.number]))
+        # print(X.describe(include=[pd.np.object]))
+        # print(X.describe(include=['category']))
+        # print(X.describe(include={'boolean'}))
+        X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=train_split_percent)
+        return X_train, X_test, y_train, y_test '''
+class Classifiers:
+    def datasetPreprocessing(self, X, columns_to_map):
+        X_clean=X
+        # mapowanie
+        for column_name in columns_to_map:
+            # konstruowanie mappera
+            mapper = {}
+            for index, category in enumerate(X_clean[column_name].unique()):
+                mapper[category] = index
+            # mapowanie
+            X_clean[column_name] = X_clean[column_name].map(mapper)
+
+        return X_clean
+#   def trainAndTestClassifier(self, clf, X_train, X_test, y_train):
+
+c=Classifiers()
+X_clean = c.datasetPreprocessing(
+     X = dane,columns_to_map = ['workclass','education','age','marital-status','occupation',
+                                'relationship', 'race', 'sex', 'native-country', 'y'])
+print(X_clean.dtypes)
+
+
 #MAPOWANIE#
 
 ''' def splitDatasetIntoTrainAndTest(self, X, y, train_split_percent = 0.6):
